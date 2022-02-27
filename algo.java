a969b7ff6bd9da8808056e18af3c155dd25a9eb3
Lakhan Pal <lakhanpal185@gmail.com>
2022-02-27 12:15:12 +0530

    algo is added

---
+++ algo.java
@@ -0,0 +1,12 @@
+public class algo {
+ 
+    public static void main(String args[])
+    {
+        int a = 2;
+        int b = 8;
+        if(a > b)
+        a = b;
+
+        System.out.println("minimum is "+ a);
+    }
+}
